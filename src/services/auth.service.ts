f847e31c19c444f8ef919d08920b622bdd001cb6
Ryuze <alfianyunianto74@gmail.com>
2025-09-02 09:10:38 +0700

    update req

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,6 +3,8 @@ import type { LoginPayload } from "../types/auth.type"
 
 export const AuthService = {
   login(payload: LoginPayload) {
-    return axios.post("/api/v1/login", payload)
+    return axios.post("/api/v1/login", payload, {
+      withCredentials: true
+    })
   },
 }
